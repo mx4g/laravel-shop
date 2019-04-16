292441fa193887fc06df9bd7d68957340cc82066
Qidong <522293522@qq.com>
2019-04-16 10:10:10 +0000

    收货地址列表

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Auth::routes(['verify' => true]);
 //Laravel 自带了一个名为 verified 的中间件，
 //如果一个未验证邮箱的用户尝试访问一个配置了 verified 中间件的路由，Laravel 就会提示该用户邮箱未激活
 Route::get('/', 'PagesController@root')->name('root')->middleware('verified');
+
+// auth 中间件代表需要登录，verified中间件代表需要经过邮箱验证
+Route::group(['middleware' => ['auth', 'verified']], function() {
+    Route::get('user_addresses', 'UserAddressesController@index')->name('user_addresses.index');
+});
