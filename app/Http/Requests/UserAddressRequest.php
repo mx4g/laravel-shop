662288aa6cdf0e3dd411e039bb35fc79e1a58244
Qidong <522293522@qq.com>
2019-04-16 11:02:22 +0000

    新增收货地址

---
+++ app/Http/Requests/UserAddressRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserAddressRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function attributes()
+    {
+        return [
+            'province'      => '省',
+            'city'          => '城市',
+            'district'      => '地区',
+            'address'       => '详细地址',
+            'zip'           => '邮编',
+            'contact_name'  => '姓名',
+            'contact_phone' => '电话',
+        ];
+    }
+}
